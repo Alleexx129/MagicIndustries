30f61865cca7acd0c63b1ef7cc7050427eb43daf
Alex <alleexx129@gmail.com>
2026-04-03 03:21:23 -0400

    Scale & Auto-hide mana bar

--- src/main/java/net/duodevs/magicindustries/screen/ManaHudOverlay.java
+++ src/main/java/net/duodevs/magicindustries/screen/ManaHudOverlay.java
@@ -16,24 +16,34 @@ public class ManaHudOverlay {
 
     public static final IGuiOverlay HUD_MANA = (gui, guiGraphics, partialTick, width, height) -> {
         Player player = Minecraft.getInstance().player;
-        if (player == null) {return;}
-
-        int x = width / 2;
-        int y = height;
-
-        RenderSystem.setShaderTexture(1, MANA_BAR_TEXTURE_EMPTY);
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.disableDepthTest();
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-
-        guiGraphics.blit(MANA_BAR_TEXTURE_EMPTY, x - 91, y - 58, 0, 0, 81, 8, 81, 11);
-
-        RenderSystem.setShaderTexture(0, MANA_BAR_TEXTURE_FILL);
-
+        if (player == null) return;
         int currentMana = ClientManaData.get();
-        guiGraphics.blit(MANA_BAR_TEXTURE_FILL, x - 91, y - 58, 0, 0, (int) (81 * (currentMana / 100f)), 8, 81, 11);
 
+        if (currentMana > 0) {
+
+            int x = width / 2;
+            int y = height;
+
+            int armorValue = player.getArmorValue();
+
+            int yOffset = -48;
+
+            if (armorValue > 0) {
+                yOffset -= 10;
+            }
+
+            RenderSystem.enableBlend();
+            RenderSystem.defaultBlendFunc();
+            RenderSystem.disableDepthTest();
+            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+
+            guiGraphics.blit(MANA_BAR_TEXTURE_EMPTY, x - 91, y + yOffset, 0, 0, 81, 8, 81, 11);
+
+
+
+            int fillWidth = (int) (81 * (currentMana / 100f));
+            guiGraphics.blit(MANA_BAR_TEXTURE_FILL, x - 91, y + yOffset, 0, 0, fillWidth, 8, 81, 11);
+        }
 
         RenderSystem.disableBlend();
         RenderSystem.enableDepthTest();
